e84aa20db04a6e1e8d61fc162e830a294b50b350
Goska <36965568+goskanowak@users.noreply.github.com>
2018-03-31 02:17:44 +0200

    Update comment

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-
 var allNames = femaleNames.concat(maleNames);
-
-
 var newName = prompt ('Please enter your name?');
  
-//Nie rozumiem dlaczego === -1?
+//I don't understand why === -1?
 if (allNames.indexOf(newName) === -1)  {
     console.log(allNames.push(newName));
 } else if (allNames.indexOf(newName) > -1) {
-       alert('Name already exist');
+    alert('Name already exist');
 }
 
 document.getElementById('newName').innerHTML = newName;
