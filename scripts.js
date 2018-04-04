3427bfb3c81717493fa9c10a84a22dcc6bb1623d
Małgorzata Nowak <goskanowak87@gmail.com>
2018-04-04 11:47:38 +0200

    Update scripts

--- scripts.js
+++ scripts.js
@@ -2,11 +2,11 @@ var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 var newName = prompt ('Please enter your name?');
- 
-//I don't understand why === -1?
+
 if (allNames.indexOf(newName) === -1)  {
     console.log(allNames.push(newName));
-} else if (allNames.indexOf(newName) > -1) {}
+} else { 
+    alert('Name already exist');}
 
 document.getElementById('newName').innerHTML = newName;
 
